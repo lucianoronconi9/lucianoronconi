f71eabeacff44d5fd870c929662ef259a054112f
lronconi <luciano.ronconi@sancristobal.com.ar>
2023-11-28 20:48:56 -0300

    Se agregan estilos de la pagina de contacto para mejorar la visualización

--- Retazos/frontend/src/components/pages/ContactoPage.js
+++ Retazos/frontend/src/components/pages/ContactoPage.js
@@ -1,29 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 import '../../styles/components/pages/ContactoPage.css'
 import '../../App.css'
 
 const ContactoPage = (props) => {
+
+    const initialForm = {
+        nombre: ' ',
+        email: ' ',
+        telefono: ' ',
+        mensaje: ' '
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState(' ');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true);
+        
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+
+        setSending(false);
+        setMsg(response.data.message);
+
+        if(response.data.error === false){
+            setFormData(initialForm);
+        }
+    }
+
     return (
         <main className='holder contacto'>
             <div>
                 <h2>Contacto Rápido</h2>
-                <form action="" method="" class="formulario">
+                <form onSubmit={handleSubmit} class="formulario">
                     <p>
                         <label for="nombre">Nombre</label>
-                        <input type="text" name="nombre" id="nombre" />
+                        <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} />                        
                     </p>
                     <p>
                         <label for="email">Mail</label>
-                        <input type="text" name="email" id="email" />
+                        <input type="text" name="email" value={formData.email} onChange={handleChange} />
                     </p>
                     <p>
                         <label for="telefono">Telefono</label>
-                        <input type="text" name="telefono" id="telefono" />
+                        <input type="text" name="telefono" value={formData.telefono} onChange={handleChange} />
                     </p>
                     <p>
                         <label for="mensaje">Mensaje</label>
-                        <input type="text" name="mensaje" id="mensaje" />
+                        <textarea type="text" name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
                     </p>
+                    { sending ? <p>Enviando ... </p> : null }
+                    { msg ? <p>{ msg }</p> : null }
                     <p class="acciones"><input type="submit" value="Enviar" /></p>
                 </form>
            </div>
