789aec1f7231ecab59ee67353e2e3633b148ac43
lronconi <luciano.ronconi@sancristobal.com.ar>
2023-11-28 08:23:59 -0300

    Logueo, routeo para usuario logueado, enmaquetado de hbs para visualizarlo mejor

--- Retazos/backend/app.js
+++ Retazos/backend/app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/novedades');
 
 var app = express();
 
@@ -19,8 +23,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use( session({
+  secret: 'asderwregdfdswtuohmn',
+  resave: false,
+  saveUninitialized : true,
+  cookie: { maxAge: null }
+}));
+
+secured = async (req, res, next) => {
+  try {
+    if(req.session.id_usuario) {
+      next();
+    } else {
+      res.redirect('./login');
+    }
+  } catch ( error ) {
+    console.log(error);
+  }
+}
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured ,adminRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
